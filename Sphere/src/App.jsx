1d63a5458eab3b5f4cdedc185db0509a17e46197
Hariskp <haris22461238@gmail.com>
2023-11-27 21:22:44 +0700

    edit App.jsx

--- Sphere/src/App.jsx
+++ Sphere/src/App.jsx
@@ -1,11 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [count, setCount] = useState(0);
+  const name = "Haris";
   return (
     <>
       <div>
@@ -25,11 +25,9 @@ function App() {
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <p className="read-the-docs">{name}</p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
