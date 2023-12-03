bf900db3e46c15cffb996e06c0580dcd309b6386
JittawatPdsr <116266763+Jittawatpdsr@users.noreply.github.com>
2023-12-03 21:57:38 +0700

    New Footer JP

--- Sphere/src/components/Footer.jsx
+++ Sphere/src/components/Footer.jsx
@@ -1,61 +1,90 @@
-import React from "react";
-import './Footer.css'
+import React from 'react' ;
+import fb from '../img/fb.png';
+import insta from '../img/insta.png';
+import linkedin from  '../img/Linkedin.png';
+import twitter from '../img/twitter.png';
+import './Footer.css';
 
-
-function Footer() {
-  return(
-   <>
-     <div className="background">
+const Footer = () => {
+  return (
     <div className="footer">
-      <div className="footer-con">
-        <div className="footer-box1"><img src="src/img/Logo.png" alt="#" /><h2>SPHERE</h2>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam amet molestiae laboriosam expedita labore eos consequuntur nihil ipsa adipisci deleniti.</div>
-        <div className="footer-box2"> <h3>PRODUCT</h3>
-        <ul>
-          <li>
-            Home
-          </li>
-          <li>
-            Blog
-          </li>
-          <li>
-            News
-          </li>
-          <li>
-            Warning
-          </li>
-          <li>
-            Weatherforecast
-          </li>
-        </ul>
+      <div className="sb_footer section_padding">
+        <div className="sb_footer-links">
+          <div className="sb_footer-links_div">
+            <h4>PRODUCT</h4>
+            <a href="/employer">
+              <p>HOME</p>
+            </a>
+            <a href="/healthplan">
+              <p>NEWS</p>
+            </a>
+            <a href="/individual">
+              <p>BLOG</p>
+            </a>
+            <a href="/individual">
+              <p>WARNING</p>
+            </a>
+            <a href="/individual">
+              <p>WEATHERFORECAST</p>
+            </a>
+          </div>
+          <div className="sb_footer-links_div">
+            <h4>ABOUT</h4>
+            <a href="/resources">
+              <p>Resources center</p>
+            </a>
+            <a href="/resources">
+              <p>Testimotials</p>
+            </a>
+            <a href="/resources">
+              <p>STV</p>
+            </a>
+          </div>
+          <div className="sb_footer-links_div">
+            <h4>ABOUT US</h4>
+            <a href="/about">
+              <p>ABOUT SPHERE</p>
+            </a>
+            <a href="/press">
+              <p>ABOUT TEAM</p>
+            </a>
+            <a href="/career">
+              <p>Career</p>
+            </a>
+            <a href="/contact">
+              <p>Contact</p>
+            </a>
+          </div>
+          <div className="sb_footer-links_div">
+            <h4>Coming soon on</h4>
+            <div className="socialmedia">
+                  <p><img src={fb} alt="" /></p>
+                  <p><img src={insta} alt="" /></p>
+                  <p><img src={linkedin} alt="" /></p>
+                  <p><img src={twitter} alt="" /></p>
+            </div>
+          </div>
         </div>
-        <div className="footer-box3"><h3>CONTACT</h3>
-        <ul>
-          <li>
-            Patumthani, Rangsit 11100, Thailand.
-          </li>
-          <li>
-            Sphereweather@sw.com
-          </li>
-          <li>
-            (+66) 123 456 789
-          </li>
-          <li>
-            (+66) 123 456 789
-          </li>
-        </ul>
-        </div>
-      </div>
-    </div>
-    </div>
-    <div className="copy-background">
-      <div className="copy-right">
-      ® 2023 Copyright Designed by Sphere 
-      </div>
-    </div>
-   </>
-  );
 
+        <hr></hr>
+
+        <div className="sb_footer-below">
+          <div className="sb_footer-copyright">
+            <p>
+            ® {new Date().getFullYear()} Sphere.  All right reserved.
+            </p>
+          </div>
+          <div className="sb_footer-below-links">
+            <a href="/terms"><div>Terms & Conditions</div></a>
+            <a href="/privacy"><div>Privacy</div></a>
+            <a href="/security"><div>Security</div></a>
+            <a href="/cookie"><div>Cookie Declaration</div></a>
+          </div>
+        </div>
+
+      </div>
+    </div>
+  );
 }
 
-export default Footer
+export default Footer;
