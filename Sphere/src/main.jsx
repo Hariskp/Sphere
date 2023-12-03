6cf69bc365bca4f35dd0170e98235c53084dabbd
Hariskp <haris22461238@gmail.com>
2023-12-03 23:14:30 +0700

    Update Main by Hariskp
    - Create folder pages
    - Create Weather.jxs, Weather.css
    - Update main.jsx (ReactDOM set router)

--- Sphere/src/main.jsx
+++ Sphere/src/main.jsx
@@ -1,10 +1,25 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+import Weather from "./pages/Weather.jsx";
+
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/Weather",
+    element: <Weather />,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
---
+++ Sphere/src/pages/Weather.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Weather = () => {
+  return (
+    <div>
+      <h1>This is Weather Page</h1>
+    </div>
+  );
+};
+
+export default Weather;
