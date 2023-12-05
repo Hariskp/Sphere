442d87033eb8c9b20387a6848523840822a529ba
Hariskp <haris22461238@gmail.com>
2023-12-05 22:34:46 +0700

    edit Weather.jst, Weather.css

--- Sphere/src/pages/Weather.jsx
+++ Sphere/src/pages/Weather.jsx
@@ -29,7 +29,23 @@ const Weather = () => {
             </div>
           </div>
           <div className="map-container">
-            <div className="map">Map Test</div>
+            <div className="map">
+              <iframe
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3870.637133811969!2d100.61278967516442!3d14.039510686383029!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x30e2801cc4ec2aff%3A0xbfec6d0816ab0b14!2z4Lih4Lir4Liy4Lin4Li04LiX4Lii4Liy4Lil4Lix4Lii4LiB4Lij4Li44LiH4LmA4LiX4Lie!5e0!3m2!1sth!2sth!4v1701759051680!5m2!1sth!2sth"
+                width="500"
+                height="350"
+                allowfullscreen=""
+                loading="lazy"
+                referrerpolicy="no-referrer-when-downgrade"
+              ></iframe>
+            </div>
+          </div>
+          <div className="uv-container">
+            <div className="uv-index">UV Index</div>
+            <div className="uv">3</div>
+            <div className="uv-rate">Low</div>
+            <div className="humidity">Humidity</div>
+            <div className="humidity-rate">84%</div>
           </div>
         </div>
       </main>
