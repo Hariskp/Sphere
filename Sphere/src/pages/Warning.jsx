41b4c8d3fc36d2743c8ed0c9c3b8f8b61eca65cb
JittawatPdsr <116266763+Jittawatpdsr@users.noreply.github.com>
2023-12-06 12:03:00 +0700

    warning update
    search bar glowing  , recent warning article , old warning article blog

--- Sphere/src/pages/Warning.jsx
+++ Sphere/src/pages/Warning.jsx
@@ -9,7 +9,20 @@ function Warning() {
                 <img src={BannerImage} alt="banner" />
                 <input className='search-warn' type="text" placeholder='Searching here...'/>
             </div>
+            <div className="recent-font">
+                <h1>Recent Warning</h1>
             </div>
+            <div className="recent-container">
+
+            </div>
+            <div className="old-container">
+                <div className="card-1">
+                    <h1>Warning</h1>
+                </div>
+                <div className="card-2">2</div>
+                <div className="card-3">3</div>
+            </div>
+        </div>
     )
 }
 
