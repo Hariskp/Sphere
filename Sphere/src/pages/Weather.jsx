501d1abdfcf09e497c4269abe9bc9c74214870fb
[punyakon.p] <Punyakon857@gmail.com>
2023-12-07 22:19:44 +0700

    Mew commit

--- Sphere/src/pages/Weather.jsx
+++ Sphere/src/pages/Weather.jsx
@@ -25,72 +25,74 @@ const Weather = () => {
               className="search-bar-weather"
             />
           </div>
-          <div className="weather-container">
-            <div className="temperature-container">
-              <div className="date">Fri 2 Dec 2023</div>
-              <div className="container-temp">
-                <div className="temperature">30˚</div>
-                <div className="img-temp">
-                  <img src={icosun} alt="sun" />
-                </div>
-              </div>
-              <div className="location">Bangkok, Thailand</div>
-            </div>
-            <div className="map-container">
-              <div className="map">
-                <iframe
-                  src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3870.637133811969!2d100.61278967516442!3d14.039510686383029!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x30e2801cc4ec2aff%3A0xbfec6d0816ab0b14!2z4Lih4Lir4Liy4Lin4Li04LiX4Lii4Liy4Lil4Lix4Lii4LiB4Lij4Li44LiH4LmA4LiX4Lie!5e0!3m2!1sth!2sth!4v1701759051680!5m2!1sth!2sth"
-                  width="500"
-                  height="350"
-                  allowfullscreen=""
-                  loading="lazy"
-                  referrerpolicy="no-referrer-when-downgrade"
-                ></iframe>
-              </div>
-            </div>
-            <div className="uv-container">
-              <div className="uv-index">
-                <h3>UV Index</h3>
-              </div>
-              <div className="container-uv">
-                <div className="uv">3</div>
-                <div className="uv-rate">Low</div>
-              </div>
-              <div className="humidity">Humidity</div>
-              <div className="humidity-rate">84%</div>
-            </div>
-            <div className="wind-status-container">
-              <div className="wind-status">WIND STATUS</div>
-              <div className="container-wind-status">
-                <div className="wind">3</div>
-                <div className="unit">KM/H</div>
-              </div>
-              <div className="container-sunrise">
-                <div className="sunrise">Sunrise</div>
-                <div className="sunrise-time">06:26</div>
-              </div>
-              <div className="container-sunset">
-                <div className="sunset">Sunset</div>
-                <div className="sunset-time">17:28</div>
-              </div>
-              <div className="container-air-pollution">
-                <div className="air-pollution">Air Pollution</div>
-                <div className="air-pollution-rate">Good</div>
-                <div className="pm">PM2.5</div>
-                <div className="pm-rate">73</div>
-              </div>
-            </div>
-            <div className="visibility-container">
-              <div className="container-visibility">
-                <div className="visibility-line">
-                  <div className="visibility">VISIBILITY</div>
-                  <div className="img-visibility">
-                    <img src={icoeye} alt="visibility" />
+          <div className="anmewza-container">
+            <div className="weather-container">
+              <div className="temperature-container">
+                <div className="date">Fri 2 Dec 2023</div>
+                <div className="container-temp">
+                  <div className="temperature">30˚</div>
+                  <div className="img-temp">
+                    <img src={icosun} alt="sun" />
                   </div>
                 </div>
-                <div className="visibility-range">27</div>
-                <div className="visibility-unit">km</div>
-                <div className="visibility-rate">Perfectly clear view.</div>
+                <div className="location">Bangkok, Thailand</div>
+              </div>
+              <div className="map-container">
+                <div className="map">
+                  <iframe
+                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3870.637133811969!2d100.61278967516442!3d14.039510686383029!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x30e2801cc4ec2aff%3A0xbfec6d0816ab0b14!2z4Lih4Lir4Liy4Lin4Li04LiX4Lii4Liy4Lil4Lix4Lii4LiB4Lij4Li44LiH4LmA4LiX4Lie!5e0!3m2!1sth!2sth!4v1701759051680!5m2!1sth!2sth"
+                    width="500"
+                    height="350"
+                    allowfullscreen=""
+                    loading="lazy"
+                    referrerpolicy="no-referrer-when-downgrade"
+                  ></iframe>
+                </div>
+              </div>
+              <div className="uv-container">
+                <div className="uv-index">
+                  <h3>UV Index</h3>
+                </div>
+                <div className="container-uv">
+                  <div className="uv">3</div>
+                  <div className="uv-rate">Low</div>
+                </div>
+                <div className="humidity">Humidity</div>
+                <div className="humidity-rate">84%</div>
+              </div>
+              <div className="wind-status-container">
+                <div className="wind-status">WIND STATUS</div>
+                <div className="container-wind-status">
+                  <div className="wind">3</div>
+                  <div className="unit">KM/H</div>
+                </div>
+                <div className="container-sunrise">
+                  <div className="sunrise">Sunrise</div>
+                  <div className="sunrise-time">06:26</div>
+                </div>
+                <div className="container-sunset">
+                  <div className="sunset">Sunset</div>
+                  <div className="sunset-time">17:28</div>
+                </div>
+                <div className="container-air-pollution">
+                  <div className="air-pollution">Air Pollution</div>
+                  <div className="air-pollution-rate">Good</div>
+                  <div className="pm">PM2.5</div>
+                  <div className="pm-rate">73</div>
+                </div>
+              </div>
+              <div className="visibility-container">
+                <div className="container-visibility">
+                  <div className="visibility-line">
+                    <div className="visibility">VISIBILITY</div>
+                    <div className="img-visibility">
+                      <img src={icoeye} alt="visibility" />
+                    </div>
+                  </div>
+                  <div className="visibility-range">27</div>
+                  <div className="visibility-unit">km</div>
+                  <div className="visibility-rate">Perfectly clear view.</div>
+                </div>
               </div>
             </div>
           </div>
