558b7370ec65b9a018ae8e7071fccfce5e3906a5
chanutkid <chanut.kidw@gmail.com>
2023-12-07 14:01:40 +0700

    Edit Header, Homepage by nutkid

--- Sphere/src/components/Header.jsx
+++ Sphere/src/components/Header.jsx
@@ -3,6 +3,17 @@ import './Header.css' ;
 import logo from '../img/Spherelogo.png';
 
 function Header() {
+    const toggle_menu = (event) => {
+        const toggle_menu = event.currentTarget;
+        toggle_menu.classList.toggle("change");
+        
+        let myMenu = document.getElementById('myMenu');
+        if (myMenu.className === 'menu') {
+            myMenu.className += ' menu-active';
+        } else {
+            myMenu.className = 'menu';
+        }
+    ;}
   return (
     <>
         <nav>
@@ -13,7 +24,7 @@ function Header() {
                         <h4>SPHERE</h4>
                         <p>Weather forecast website</p>
                     </div>
-                    <ul class="menu">
+                <ul class="menu" id="myMenu">
                     <li><a href="#">Home</a></li>
                     <li><a href="#">Weatherforecast</a></li>
                     <li><a href="#">Warning</a></li>
@@ -21,6 +32,11 @@ function Header() {
                     <li><a href="#">Blog</a></li>
                     <li><a href="#">About us</a></li>
                 </ul>
+                <div className="menu-icon" onClick={toggle_menu}>
+                    <div className="bar1"></div>
+                    <div className="bar2"></div>
+                    <div className="bar3"></div>
+                </div>
                 </div>
             </div>
         </nav>
